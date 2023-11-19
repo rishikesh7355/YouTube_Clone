93cd7fb5eb532db571c1533eb3211c9da1427a83
rishikesh7355 <rishiyadav280198@gmail.com>
2023-11-19 22:18:55 +0530

    sidebar 4items add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Header'
+import Sidebar from './Sidebar'
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Sidebar/>
     </div>
   );
 }
